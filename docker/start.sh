8f7fc46434df27b49c8dd7d3c3de782b47408a4c
Alexander Paschke <alexsis20102@gmail.com>
2026-02-25 17:50:04 +0100

    Run Docker container in CI

--- docker/start.sh
+++ docker/start.sh
@@ -19,5 +19,5 @@ php artisan migrate --force
 echo "Seeding database..."
 php artisan db:seed --force
 
-echo "Starting PHP-FPM..."
-php-fpm
+echo "Starting Laravel server..."
+php artisan serve --host=0.0.0.0 --port=8000
